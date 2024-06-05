06895607f2c257eeb660642bb75582b7ba0d8cce
Ahmed Hemdan <beyada85@gmail.com>
2024-06-05 16:24:11 +0300

    fixing key issue in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,8 +31,8 @@ const Footer = () => {
             <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Dreamer</p>
             <div className="flex items-center md:gap-3 gap-6">
                 {socialMedia.map((profile) => (
-                    <a href="https:www.google.com" target="_blank">
-                    <div  key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 dark:bg-black-300 bg-purple rounded-lg border dark:border-black-300 border-slate-400 hover:bg-pink-300 transition-all">
+                    <a key={profile.id} href="https:www.google.com" target="_blank">
+                    <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 dark:bg-black-300 bg-purple rounded-lg border dark:border-black-300 border-slate-400 hover:bg-pink-300 dark:hover:bg-black-100 transition-all">
                         <img src={profile.img} alt={"social link"} width={20} height={20} />
                     </div>
                     </a>
