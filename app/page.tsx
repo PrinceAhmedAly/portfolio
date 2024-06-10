f059d91a7d1659feda53d95bb492f32db555ab58
Ahmed Hemdan <beyada85@gmail.com>
2024-06-10 18:38:07 +0300

    change darkmode btn

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="relative dark:bg-black-100 bg-white flex justify-center items-center flex-col mx-auto sm:px-10 overflow-clip px-5">
       <div className="max-w-7xl w-full">
-        <div className="fixed top-10 left-10 z-50">
+        <div className="fixed top-10 left-5 md:left-10 z-50">
         <ModeToggle />
         </div>
         <FloatingNav 
